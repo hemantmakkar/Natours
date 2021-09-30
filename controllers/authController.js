99a34f51fae6243b3d1eec89f44cd4b5e4a76a3e
Hemant Makkar <gautam.makkar2001@gmail.com>
2021-09-30 19:17:47 +0530

    Modified mailing

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,8 +49,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const url = `${req.protocol}://${req.get('host')}/me`;
-  await new Email(newUser, url).sendWelcome();
+  // const url = `${req.protocol}://${req.get('host')}/me`;
+  // await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, req, res);
 });
